fbd4d76e911686267e55889817d4f21361dfca7e
Rohan-218 <ntomar313@gmail.com>
2024-09-30 16:00:12 +0530

    TeacherManagement API

--- server/src/routes/teacherManagementRoutes.js
+++ server/src/routes/teacherManagementRoutes.js
@@ -1,7 +1,15 @@
 const express = require('express');
-const { getTeacherCount } = require('../controllers/teacherManagementController')
+const { getTeacherCount, getAllTeachers, getTeachersByBranchAndSemester, searchTeachersByName } = require('../controllers/teacherManagementController')
 const router = express.Router();
 
 router.get('/count', getTeacherCount);
+
+router.get('/', getAllTeachers);
+
+// API to get teachers by branch and semester
+router.get('/branch-semester', getTeachersByBranchAndSemester);
+
+// API to search teachers by name
+router.get('/search', searchTeachersByName);
 
 module.exports = router;
--- server/src/services/teacherManagementService.js
+++ server/src/services/teacherManagementService.js
@@ -1,4 +1,4 @@
-const { getTeacherCount } = require('../models/teacherManagementModel');
+const { getTeacherCount, getAllTeachers, getTeachersByBranchAndSemester, searchTeachersByName } = require('../models/teacherManagementModel');
 
 exports.getTeacherCount = async () => {
     try {
@@ -9,4 +9,29 @@ exports.getTeacherCount = async () => {
       throw new Error("Failed to fetch teacher count.");
     }
 };
-  
+
+exports.getAllTeachers = async () => {
+  try {
+    return await getAllTeachers();
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+// Fetch teachers by branch and semester
+exports.getTeachersByBranchAndSemester = async (branchName, semester) => {
+  try {
+    return await getTeachersByBranchAndSemester(branchName, semester);
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+// Search teachers by name
+exports.searchTeachersByName = async (name) => {
+  try {
+    return await searchTeachersByName(name);
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
