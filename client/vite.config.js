04ccc135392dc0d13777d2ffd466049ebbb8a9c9
Rohan-218 <ntomar313@gmail.com>
2024-10-03 16:45:25 +0530

    🫡🫡

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // This makes the development server accessible externally
+    port: 5173,      // Change the port if necessary
+  },
 })
