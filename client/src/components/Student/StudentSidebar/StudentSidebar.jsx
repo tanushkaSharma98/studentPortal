afb976b11190ba9583d969f103db4d426670d777
Aditya916Singh <aditya0singhchauhan@gmail.com>
2024-10-03 15:03:54 +0530

    icon pic added

--- client/src/components/Student/StudentSidebar/StudentSidebar.jsx
+++ client/src/components/Student/StudentSidebar/StudentSidebar.jsx
@@ -1,13 +1,24 @@
-// src/components/Student/StudentSidebar/StudentSidebar.jsx
 import React from 'react';
 import './StudentSidebar.css';
+import dashboard from '/src/assets/TeacherSidebar_icon/dashboard.png'
+import scoreboard from  '/src/assets/TeacherSidebar_icon/scoreboard.png' 
+import attendance from  '/src/assets/TeacherSidebar_icon/attendant.png'
 
 const StudentSidebar = ({ onScroll }) => {
   return (
     <div className="Sidebar">
-      <button onClick={() => onScroll('dashboard')}>Dashboard</button>
-      <button onClick={() => onScroll('scoreboard')}>Scoreboard</button>
-      <button onClick={() => onScroll('attendance')}>Attendance</button>
+      <button onClick={() => onScroll('dashboard')}>
+        <img src={dashboard} alt="Dashboard Icon" className="icon" />
+        Dashboard
+      </button>
+      <button onClick={() => onScroll('scoreboard')}>
+        <img src={scoreboard} alt="Scoreboard Icon" className="icon" />
+        Scoreboard
+      </button>
+      <button onClick={() => onScroll('attendance')}>
+        <img src={attendance} alt="Attendance Icon" className="icon" />
+        Attendance
+      </button>
     </div>
   );
 };
