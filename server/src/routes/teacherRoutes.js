544de5b42bde2077174b5ddb14f464a95fe357fa
Rohan-218 <ntomar313@gmail.com>
2024-10-04 19:29:27 +0530

    ../

--- server/src/routes/teacherRoutes.js
+++ server/src/routes/teacherRoutes.js
@@ -1,21 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { getTeacherProfile, uploadAttendance, getUploadedAttendance } = require('../controllers/teacherController.js');
-
 const teacherSubController = require('../controllers/teacherSubController');
-// Route to get student data
-router.get('/profile', getTeacherProfile);
-
 const teacherSubStudentController = require('../controllers/teacherSubStudentController');
 
-router.get('/subject-students', (req, res, next) => {
-  console.log("Received GET request:", req.query); // Log query parameters
-  next();
-}, teacherSubStudentController.getStudentsBySubject);
+router.get('/profile', getTeacherProfile);
 
-// // Route to get students by subject code
-// router.get('/subject-students', teacherSubStudentController.getStudentsBySubject);
-
+router.get('/subject-students', teacherSubStudentController.getStudentsBySubject);
 
 // POST route to upload attendance
 router.post('/attendance/upload', uploadAttendance);
@@ -25,7 +16,5 @@ router.get('/attendance/get', getUploadedAttendance);
 
 // Define route to get subjects for a specific teacher
 router.get('/subjects', teacherSubController.getSubjectsByTeacher);
-
-
 
 module.exports = router;
